8667e698e0e2a4fe99b01534dae12aca5ab0f730
Jirat Kaennak <mr.pkisz@gmail.com>
2021-12-21 11:29:02 +0700

    Version 1.0.0

---
+++ testfunction.php
@@ -0,0 +1,8 @@
+<?php
+    include"all_function.php";//เพื่อให้สามารถเรียกใช้ฟังก์ชันได้
+    echo "<center>";
+    echo "นี่คือข้อความปกติ ที่อยู่ในไฟล์ testfunction.php";
+    echo "<hr>";
+    echo "</center>";
+    showmessage();//เรียกใช้ฟังก์ชัน showmessage บรรทัดที่ 2
+?>
